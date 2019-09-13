9d096a8604593a267b994f72df6175978c122f19
Jeremy Pry <jeremy.pry@gmail.com>
2019-09-13 10:59:27 -0400

    Initial plugin

---
+++ jpry-workshop.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: JPry Gutenberg Workshop plugin
+ * Plugin URI:
+ * Description:
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+add_action( 'init', function() {
+} );
